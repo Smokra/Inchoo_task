02d24d592456554dd41a6182a5fb22cf7974f7cb
smodev <ivan.smokrovic@gmail.com>
2021-01-22 13:07:21 +0100

    Fixing Validation issues, removing GSAP and making functions in vanilla JS

--- script.js
+++ script.js
@@ -1,60 +1,20 @@
 function openCartBox() {
-  const x = document.querySelector("#cartBox");
-  if (x.style.display === "flex") {
-    x.style.display = "none";
+  const cartBox = document.querySelector("#cartBox");
+  if (cartBox.style.display === "flex") {
+    cartBox.style.display = "none";
   } else {
-    x.style.display = "flex";
+    cartBox.style.display = "flex";
   }
 }
 
-const wrapper = document.querySelectorAll(".content")[0];
+const prev = document.querySelector('#prevButton');
+const next = document.querySelector('#nextButton');
+const items = document.querySelector('.items');
 
-const up = document.querySelector("#prevButton");
-const down = document.querySelector("#nextButton");
-
-const items = document.querySelector(".items");
-const products = document.querySelectorAll(".products");
-const boxHeight = 116;
-const wrapHeight = 696; //(items.height - 1) * boxHeight;
-const viewHeight = 348; //wrapper.offsetHeight;
-
-//gsap.set(wrapper, { height: 696 });
-//gsap.set(items, { y: (i) => i * boxHeight });
-
-const animation = gsap.to(".items", {
-  duration: 1,
-  y: "+=" + (wrapHeight + boxHeight),
-  ease: "easeOut",
-  paused: true,
+prev.addEventListener('click', () => {
+  items.scrollTop -= 116;
 });
 
-const proxy = document.createElement("div");
-
-Draggable.create(proxy, {
-  type: "y",
-  trigger: "#wrapper",
-  throwProps: true,
-  onDrag: updateProgress,
-  onThrowUpdate: updateProgress,
-  snap: {
-    y: gsap.utils.snap(boxHeight)
-  }
+next.addEventListener('click', () => {
+  items.scrollTop += 116;
 });
-
-function updateProgress() {
-  const val = this.y / wrapHeight;
-  animation.progress(gsap.utils.wrap(0, 1, val));
-}
-
-const animatePrev = animateCarousel.bind(up, -boxHeight);
-const animateNext = animateCarousel.bind(down, boxHeight);
-
-function animateCarousel(delta) {
-  gsap.to(items, {
-    duration: 0.2,
-    y: "+=" + delta,
-  });
-}
-
-up.addEventListener("click", animatePrev);
-down.addEventListener("click", animateNext);
